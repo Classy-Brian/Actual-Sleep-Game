9e2dd8aeaf067975f5f14e431d7d559242d22d0d
Classy-Brian <phanbrian101@gmail.com>
2026-08-09 20:37:21 -0700

    Update collision for level 1 and added hud

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,8 +3,15 @@ extends CanvasLayer
 var player
 
 @onready var fade_overlay: ColorRect = $FadeOverlay
+@onready var pillow_counter: Label = $PillowCounter
 
 func fade(to_alpha: float) -> void:
 	var tween:= create_tween()
 	tween.tween_property(fade_overlay, "modulate:a", to_alpha, 1.5)
 	await tween.finished
+
+func _process(delta: float) -> void:
+	if PlayersStats.keys_collected >= PlayersStats.keys_required:
+		pillow_counter.text = "Go to bed!"
+	else:
+		pillow_counter.text = "Pillows: " + str(PlayersStats.keys_collected) + " / " + str(PlayersStats.keys_required) + " to sleep"
